8ab36883d1a53ea533195ee367424b2dc95086af
vinayramteke <22bee120@nith.ac.in>
2025-11-22 17:31:37 +0530

    refactor: update rendering method to use JSX for improved readability

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //step 0: Import whatever u will going to use
 import React from "react";
 import ReactDOM from "react-dom/client";
-//step 1: what you whant to render
+//step 1: what you whant to render ; using React.createElement
 const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+//step 1: what you whant to render ; using JSX
+const jsxHeading = <h1 id="heading">Namaste React By JSX</h1>;
 //step 2: where you whant to render
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //step 3: render
-root.render(heading);
+root.render(jsxHeading);

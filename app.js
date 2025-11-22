2305546a94bbd9a05e61dfc45bbe2aa889aa76e3
vinayramteke <22bee120@nith.ac.in>
2025-11-22 22:55:46 +0530

    refactor: update HeadingComponent to include Title component for improved structure

--- app.js
+++ app.js
@@ -8,13 +8,21 @@ const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
 //step 1: what you whant to render ; using JSX
 const jsxHeading = <h1 id="heading">Namaste React By JSX</h1>;
 
+//compenent composition: importing one react component into other react component
+const Title = () => <h1 className="title">This is my title compenent</h1>;
+
 //React Component : functional Component => A js arrow function which return JSX
-const HeatingComponent = () => (
-  <h1 className="heading">Namaste React using FN Component</h1>
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 id="heading" className="heading">
+      Namaste React using FN Component
+    </h1>
+  </div>
 );
 
 //step 2: where you whant to render
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //step 3: render
-root.render(<HeatingComponent />);
+root.render(<HeadingComponent />);

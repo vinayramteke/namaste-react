785aa7103a69058dbc2e1ca030c19f19e2ff973b
vinayramteke <22bee120@nith.ac.in>
2025-11-23 18:00:39 +0530

    refactor: restructure Header component and update AppBody for improved layout

--- app.js
+++ app.js
@@ -2,30 +2,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//step 1: what you whant to render ; using React.createElement
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo-img"
+          src="https://s.tmimgcdn.com/scr/1200x627/346100/food-creative-logo-template_346101-original.jpg"
+        ></img>
+      </div>
+      <div className="nav-container">
+        <ul className="nav-items">
+          <li className="nav-link">Home</li>
+          <li className="nav-link">About Us</li>
+          <li className="nav-link">Contact Us</li>
+          <li className="nav-btn">Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-//step 1: what you whant to render ; using JSX
-const jsxHeading = <h1 id="heading">Namaste React By JSX</h1>;
-
-//compenent composition: importing one react component into other react component
-const Title = () => <h1 className="title">This is my title compenent</h1>;
-
-//React Component : functional Component => A normal js function which return JSX
-const HeadingComponent = () => (
-  <div id="container">
-    {/* using react compenent */}
-    <Title />
-    {/* using {} */}
-    {jsxHeading}
-    <h1 id="heading" className="heading">
-      Namaste React using FN Component
-    </h1>
-  </div>
-);
+const AppBody = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
 
 //step 2: where you whant to render
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //step 3: render
-root.render(<HeadingComponent />);
+root.render(<AppBody />);

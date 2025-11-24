363392a453fc609f1d0c510eabbd08040f76e399
vinayramteke <22bee120@nith.ac.in>
2025-11-24 16:04:49 +0530

    refactor: implement search and filter functionality in Body component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,10 @@
 import RestroCard from "./RestroCard";
 import restroList from "../utils/restroData";
+import { useState } from "react";
 
 //step 2:building Body for my food app
 const Body = () => {
+  const [listOfRestro, setlistOfRestro] = useState(restroList);
   return (
     <div className="app-body">
       <div className="search-container">
@@ -12,10 +14,22 @@ const Body = () => {
           placeholder="Search for restaurants..."
         />
         <button className="search-btn">Search</button>
+        <button
+          className="filter-button"
+          onClick={() => {
+            const filteredList = listOfRestro.filter(
+              (restaurantes) => restaurantes.avgRating > 4
+            );
+            setlistOfRestro(filteredList);
+          }}
+        >
+          Most Rated Restaurant
+        </button>
       </div>
+      <div className="filter-container"></div>
       <div className="restro-container">
-        {restroList.map((restaurant) => (
-          <RestroCard key={restaurant.info.id} restroInfo={restaurant} />
+        {listOfRestro.map((restaurant) => (
+          <RestroCard key={restaurant.id} restroInfo={restaurant} />
         ))}
       </div>
     </div>

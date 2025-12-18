e93c0e92378d22624393fd4dfd2eff4f624e621b
vinayramteke <22bee120@nith.ac.in>
2025-12-18 15:08:42 +0530

    added style to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,27 +7,35 @@ const Header = () => {
   const [btnName, setBtnName] = useState("Login");
 
   return (
-    <div className="header">
-      <div className="logo-container">
-        <img className="logo-img" src={LOGO_URL}></img>
+    <div
+      className="header-container py-4 px-8 flex justify-between items-center top-0 sticky bg-[#ffffff] shadow-[0_4px_6px_-1px_rgba(0, 0, 0, 0.1) z-[1000];
+]"
+    >
+      <div className="logo-container w-15 cursor-pointer block ">
+        <img
+          className="logo-img w-full aspect-square object-cover rounded-full"
+          src={LOGO_URL}
+        ></img>
       </div>
+
       {/* Do not use <a></a> tag to link pages as it reload whole body of webApp use{" "}
       <Link></Link> provide by react-router as it only refrsh component which
       get updated */}
-      <div className="nav-container">
-        <ul className="nav-items">
-          <li className="nav-link">
+
+      <div className="nav-container  ">
+        <ul className="nav-items flex  cursor-pointer  items-center gap-8 m-0 p-0 font-semibold text-[#4b5563] ">
+          <li className="nav-link hover:text-[#f97316]">
             <Link to="/">Home</Link>
           </li>
-          <li className="nav-link">
+          <li className="nav-link hover:text-[#f97316]">
             <Link to="/about">About Us</Link>
           </li>
-          <li className="nav-link">
+          <li className="nav-link hover:text-[#f97316]">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li className="nav-btn">Card</li>
+          <li className="nav-btn ">Cart</li>
           <li
-            className="login-btn"
+            className="login-btn "
             onClick={() => {
               btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
             }}
